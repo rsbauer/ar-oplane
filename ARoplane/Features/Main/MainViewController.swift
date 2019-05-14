3276acf04575f2d4b33054a3c34e1f1e9a39c213
Robert Bauer <rbauer1337@gmail.com>
2019-05-14 07:33:03 -0400

    Add ARCL to view controller

--- ARoplane/Features/Main/MainViewController.swift
+++ ARoplane/Features/Main/MainViewController.swift
@@ -6,72 +6,26 @@
 //  Copyright © 2019 RSB. All rights reserved.
 //
 
-import ARKit
-import SceneKit
+import ARCL
+import CoreLocation
 import UIKit
 
-public class MainViewController: UIViewController, ARSCNViewDelegate {
+// See: https://github.com/ProjectDent/ARKit-CoreLocation
 
-	@IBOutlet var sceneView: ARSCNView!
+public class MainViewController: UIViewController {
 	
+	private var sceneLocationView = SceneLocationView()
+
 	public override func viewDidLoad() {
 		super.viewDidLoad()
-		
-		// Set the view's delegate
-		sceneView.delegate = self
-		
-		// Show statistics such as fps and timing information
-		sceneView.showsStatistics = true
-		
-		// Create a new scene
-		guard let scene = SCNScene(named: "art.scnassets/ship.scn") else {
-			return
-		}
-		
-		// Set the scene to the view
-		sceneView.scene = scene
+	
+		sceneLocationView.run()
+		view.addSubview(sceneLocationView)
 	}
 	
-	public override func viewWillAppear(_ animated: Bool) {
-		super.viewWillAppear(animated)
+	public override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
 		
-		// Create a session configuration
-		let configuration = ARWorldTrackingConfiguration()
-		
-		// Run the view's session
-		sceneView.session.run(configuration)
-	}
-	
-	public override func viewWillDisappear(_ animated: Bool) {
-		super.viewWillDisappear(animated)
-		
-		// Pause the view's session
-		sceneView.session.pause()
-	}
-	
-	// MARK: - ARSCNViewDelegate
-	
-	/*
-	// Override to create and configure nodes for anchors added to the view's session.
-	func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-	let node = SCNNode()
-	
-	return node
-	}
-	*/
-	
-	private func session(_ session: ARSession, didFailWithError error: Error) {
-		// Present an error message to the user
-		
-	}
-	
-	private func sessionWasInterrupted(_ session: ARSession) {
-		// Inform the user that the session has been interrupted, for example, by presenting an overlay
-		
-	}
-	
-	private func sessionInterruptionEnded(_ session: ARSession) {
-		// Reset tracking and/or remove existing anchors if consistent tracking is required
-		
+		sceneLocationView.frame = view.bounds
 	}
 }
